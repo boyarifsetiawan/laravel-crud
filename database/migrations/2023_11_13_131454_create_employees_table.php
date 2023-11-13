0dc788100b79921a30f3305427d582e68fd75be6
boyarifsetiawan <boysetiawan5635@gmail.com>
2023-11-13 20:25:25 +0700

    Create Migration

--- database/migrations/2023_11_13_131454_create_employees_table.php
+++ database/migrations/2023_11_13_131454_create_employees_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Integer;
 
 return new class extends Migration
 {
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('nama', 50);
+            $table->string('jabatan', 50);
+            $table->Integer('umur');
+            $table->text('alamat', 100);
             $table->timestamps();
         });
     }
